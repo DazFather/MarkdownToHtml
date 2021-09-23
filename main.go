859e07e9ae0b76d2b2933926c5edc0fc9d6b3fca
DazFather <39210887+DazFather@users.noreply.github.com>
2021-09-23 18:08:58 +0200

    Improved detection of tags and maintainability

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"html"
 	"regexp"
-	"fmt"
 	"strings"
 )
 
+func initRegexPatterns(patterns map[string]string) (m map[string]*regexp.Regexp, r *regexp.Regexp) {
+	var fullPattern string
+	m = make(map[string]*regexp.Regexp)
+
+	if len(patterns) > 0 {
+		for key, val := range patterns {
+			m[key] = regexp.MustCompile(val)
+			fullPattern += fmt.Sprint("|", val)
+		}
+		r = regexp.MustCompile(fullPattern[1:])
+	}
+
+	return
+}
 
 func Translate(text string) (parsed string) {
 	var (
+		// Regexp
+		rgx, rgxToken = initRegexPatterns(
+			map[string]string{
+				"hr":     `(^|\n) ?-{3} ?(\n|$)`,
+				"h":      `(^|\n)#{1,7} ?`,
+				"a":      `\[.+\]\([\w\./:%&\?!=-]+\)`,
+				"format": `[_*]{1,2}`,
+			},
+		)
+		rgxEndln = regexp.MustCompile(`\r?\n`)
+
 		tag     string
 		cursor  int
 		wait    = make(map[string]bool)
@@ -19,8 +44,6 @@ func Translate(text string) (parsed string) {
 			"__": "em",
 			"**": "strong",
 		}
-		rgxToken = regexp.MustCompile(`\n ?-{3} ?[\n$]|[_*]{1,2}|#{1,7}|\[.+\]\([\w\./:%&\?!=-]+\)`)
-		rgxEndln = regexp.MustCompile(`\r?\n`)
 	)
 
 	vals := rgxToken.FindAllStringIndex(text, -1)
@@ -32,13 +55,12 @@ func Translate(text string) (parsed string) {
 		token := text[min:max]
 
 		switch true {
-
 		case wait[token]:
 			tag = "</" + tagName[token] + ">"
 			delete(wait, token)
 
-		case token[0] == '#':
-			tag = fmt.Sprint("<h", len(token), ">")
+		case rgx["h"].MatchString(token):
+			tag = fmt.Sprint("<h", len(strings.TrimSpace(token)), ">")
 			if val := rgxEndln.FindStringIndex(text[max:]); val != nil {
 				parsed += html.EscapeString(text[cursor:min]) + tag
 				parsed += Translate(text[max:max+val[0]]) + "</" + tag[1:] + "\n"
@@ -47,7 +69,7 @@ func Translate(text string) (parsed string) {
 				continue
 			}
 
-		case token[0] == '[':
+		case rgx["a"].MatchString(token):
 			link := strings.Split(token[1:len(token)-1], "](")
 			if match, _ := regexp.MatchString(`(?i).*\.(apng|avif|gif|jpe?g|jpe|jf?if|png|svg|webp)$`, link[1]); match {
 				tag = fmt.Sprint("<img src=\"", link[1], "\" alt=\"", html.EscapeString(link[0]), "\">")
@@ -55,7 +77,7 @@ func Translate(text string) (parsed string) {
 				tag = fmt.Sprint("<a href=\"", link[1], "\">", Translate(link[0]), "</a>")
 			}
 
-		case strings.Contains(token, "---"):
+		case rgx["hr"].MatchString(token):
 			tag = "\n<hr>\n"
 
 		default:
